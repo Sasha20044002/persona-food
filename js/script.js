fed6ea8ceecb4a99447d99c05229a949fde04be7
Sca1red <40990563+Sasha20044002@users.noreply.github.com>
2021-07-26 16:13:53 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,10 +7,6 @@ const sliderHeader = new Swiper("#slider", {
     prevEl: ".swiper-button-prev",
   },
   loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false
-  }
 });
 
 $(document).ready(function () {
@@ -31,25 +27,10 @@ $(document).ready(function () {
   });
 });
 
-window.onscroll = scrollFunction();
-
-function scrollFunction() {
-  if (window.scrollTop > 70) {
-    console.log(window.offsetTop);
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-
 for (let i = 0; i < document.getElementsByClassName("label-menu").length; i++) {
   const a = document.getElementsByClassName("label-menu")[i];
   a.onclick = function () {
     if (window.innerWidth <= 1024) {
-      console.log(window.innerWidth);
       if (!document.getElementById("menu-mobile").checked) {
         document.getElementById("right").classList.remove("hidden");
         document.getElementById("navigation-list").classList.remove("hidden");
@@ -65,23 +46,219 @@ for (let i = 0; i < document.getElementsByClassName("label-menu").length; i++) {
   };
 }
 
-window.onscroll = function () {
-  if (
-    window.offsetHeight >=
-      document.getElementById("header").offsetHeight +
-        document.getElementById("slider").offsetHeight +
-        document.getElementById("popular").offsetHeight +
-        document.getElementById("restaurantNav").offsetHeight +
-        document.getElementById("restaurantTitle").offsetHeight ||
-    window.offsetHeight <=
-      document.getElementById("header").offsetHeight +
-        document.getElementById("slider").offsetHeight +
-        document.getElementById("popular").offsetHeight +
-        document.getElementById("restaurant").offsetHeight
-  ) {
-    document.getElementById("restaurantNav").classList.add("fixed");
-  } else {
-    document.getElementById("restaurantNav").classList.remove("fixed");
+// header nav
+if ($(window).scrollTop() >= 72) {
+  $("#style-1").text(`
+  #navigation {
+    position: fixed;
+    background: #ffffff;
+    width: ${$("#navigation").width()}px;
   }
-};
+  #fixed {
+    position: fixed;
+    width: 100%;
+    height: 72px;
+    background: #ffffff;
+    top: 0;
+    left: 0;
+    z-index: 6;
+    display: flex;
+    justify-content: center;
+  }
+  @media (max-width: 1024px) {
+    #navigation {
+      position: static;
+    }
+    #fixed {
+      position: static;
+      justify-content: flex-end;
+      width: 48px;
+    }
+  }
+  `);
+} else {
+  $("#style-1").text(``);
+}
 
+if ($(window).width() >= 1025) {
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() >= 72) {
+      $("#style-1").text(`
+      #navigation {
+        position: fixed;
+        background: #ffffff;
+        width: ${$("#navigation").width()}px;
+      }
+      #fixed {
+        position: fixed;
+        width: 100%;
+        height: 72px;
+        background: #ffffff;
+        top: 0;
+        left: 0;
+        z-index: 6;
+        display: flex;
+        justify-content: center;
+      }
+      @media (max-width: 1024px) {
+        #navigation {
+          position: static;
+        }
+        #fixed {
+          position: static;
+          justify-content: flex-end;
+          width: 48px;
+        }
+      }
+      `);
+    } else {
+      $("#style-1").text(``);
+    }
+  });
+}
+
+$(window).resize(function () {
+  if ($(window).width() >= 1025) {
+    $(window).on("scroll", function () {
+      if ($(window).scrollTop() >= 72) {
+        $("#style-1").text(`
+        #navigation {
+          position: fixed;
+          background: #ffffff;
+          width: ${$("#navigation").width()}px;
+        }
+        #fixed {
+          position: fixed;
+          width: 100%;
+          height: 72px;
+          background: #ffffff;
+          top: 0;
+          left: 0;
+          z-index: 6;
+          display: flex;
+          justify-content: center;
+        }
+        @media (max-width: 1024px) {
+          #navigation {
+            position: static;
+          }
+          #fixed {
+            position: static;
+            justify-content: flex-end;
+            width: 48px;
+          }
+        }
+        `);
+      } else {
+        $("#style-1").text(``);
+      }
+    });
+  }
+});
+
+console.log($("#height").height() + $("#restaurantTitle").height());
+// restaurant nav
+if (
+  $(window).scrollTop() >=
+  $("#height").height() + $("#restaurantTitle").height()
+) {
+  $("#style-2").text(`
+  #fixed-2 {
+    position: fixed;
+    background: #ffffff;
+    width: 100%;
+    top: 72px;
+    left: 0;
+    z-index: 6;
+    display: flex;
+    justify-content: center;
+  }
+  #restaurantNav{
+    margin-bottom: 0;
+    margin-left: 24px;
+  }
+  @media (max-width: 1024px) {
+    #fixed-2{top: 0;}
+  }
+  `);
+} else {
+  $("#style-2").text(`
+  #restaurantNav{
+    position: static
+  }`);
+}
+
+$(window).on("scroll", function () {
+  if (
+    $(window).scrollTop() >=
+    $("#height").height() + $("#restaurantTitle").height()
+  ) {
+    $("#style-2").text(`
+    #fixed-2 {
+      position: fixed;
+      background: #ffffff;
+      width: 100%;
+      top: 72px;
+      left: 0;
+      z-index: 6;
+      display: flex;
+      justify-content: center;
+    }
+    #restaurantNav{
+      margin-bottom: 0;
+      margin-left: 24px;
+    }
+    @media (max-width: 1024px) {
+      #fixed-2{top: 0;}
+    }
+    `);
+  } else {
+    $("#style-2").text(`
+    #restaurantNav{
+      position: static
+    }`);
+  }
+});
+
+$(window).resize(function () {
+  $(window).on("scroll", function () {
+    if (
+      $(window).scrollTop() >=
+      $("#height").height() + $("#restaurantTitle").height()
+    ) {
+      $("#style-2").text(`
+      #fixed-2 {
+        position: fixed;
+        background: #ffffff;
+        width: 100%;
+        top: 72px;
+        left: 0;
+        z-index: 6;
+        display: flex;
+        justify-content: center;
+      }
+      #restaurantNav{
+        margin-bottom: 0;
+        margin-left: 24px;
+      }
+      @media (max-width: 1024px) {
+        #fixed-2{top: 0;}
+      }
+      `);
+    } else {
+      $("#style-2").text(`
+      #restaurantNav{
+        position: static
+      }`);
+    }
+  });
+});
+
+
+for (let i = 0; i < document.getElementsByClassName('restaurant__item').length; i++) {
+  const a = document.getElementsByClassName('restaurant__item')[i];
+  a.onclick = function () {
+    // window.scrollTo(0,$('#height').height())
+    $('html, body').animate({'scrollTop': ($('#height').height() + 57)}, 500);
+  }
+}
